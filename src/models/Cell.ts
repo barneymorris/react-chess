97b975e7371126b4c4233bac201a68c09bb0380d
bsyrpin <bsyrpin@moysklad.ru>
2022-05-17 14:20:52 +0500

    Cell and Board classes

---
+++ src/models/Cell.ts
@@ -0,0 +1,30 @@
+import { nanoid } from "nanoid";
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
+
+export class Cell {
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  figure: Figure | null;
+  board: Board;
+  avaiable: boolean;
+  id: string;
+
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Colors,
+    figure: Figure | null
+  ) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.figure = figure;
+    this.board = board;
+    this.avaiable = false;
+    this.id = nanoid();
+  }
+}
