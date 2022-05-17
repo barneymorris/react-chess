10a76a091f76f47b2ca9c6445c9f4b415da046cc
bsyrpin <bsyrpin@moysklad.ru>
2022-05-17 14:28:33 +0500

    Advance in structure

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,23 @@
+import { useCallback, useEffect, useState } from "react";
+import { Board } from "../../models/Board";
 import { BoardComponent } from "../BoardComponent/BoardComponent";
 
 const App = () => {
+  const [board, setBoard] = useState(new Board());
+
+  const restart = useCallback(() => {
+    const newBoard = new Board();
+    newBoard.init();
+    setBoard(newBoard);
+  }, []);
+
+  useEffect(() => {
+    restart();
+  }, [restart]);
+
   return (
     <div className="app">
-      <BoardComponent />
+      <BoardComponent board={board} setBoard={setBoard} />
     </div>
   );
 };
--- src/components/BoardComponent/BoardComponent.tsx
+++ src/components/BoardComponent/BoardComponent.tsx
@@ -1,3 +1,22 @@
-export const BoardComponent = () => {
-  return <div className="board"></div>;
+import React from "react";
+import { Board } from "../../models/Board";
+import { CellComponent } from "../CellComponent/CellComponent";
+
+type Props = {
+  board: Board;
+  setBoard: (board: Board) => void;
+};
+
+export const BoardComponent: React.FC<Props> = ({ board, setBoard }) => {
+  return (
+    <div className="board">
+      {board.cells.map((row, idx) => (
+        <React.Fragment key={idx}>
+          {row.map((cell) => (
+            <CellComponent />
+          ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
 };
